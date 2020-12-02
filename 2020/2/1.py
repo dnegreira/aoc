4d3d02a4609fb7dea2e703d17ff4f768e7b9a155
David Negreira <david@otherreality.net>
2020-12-02 15:25:16 +0100

    remove unnecessary int conversion

--- 2020/2/1.py
+++ 2020/2/1.py
@@ -4,12 +4,13 @@ f = open("input2.txt")
 
 match = 0
 for line in f:
+    count = []
     inputl = line.split()
     for i in inputl[0].split("-"):
         count.append(int(i))
     letter = inputl[1][0]
     password = inputl[2]
-    if password.count(letter) >= int(count[0]) and password.count(letter) <= int(count[1]):
+    if password.count(letter) >= count[0] and password.count(letter) <= count[1]:
         match += 1
 
 print("Number of matches: {}".format(match))
